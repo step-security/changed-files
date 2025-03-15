2aa7c12eaeccabc0f6b203fc08594c6cb70513b3
Ashish Kurmi <akurmi@stepsecurity.io>
2025-03-15 12:48:01 -0700

    removing subscription check to make the Action available to everyone

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,6 @@ import {
   verifyMinimumGitVersion,
   warnUnsupportedRESTAPIInputs
 } from './utils'
-import axios, {isAxiosError} from 'axios'
 
 const getChangedFilesFromLocalGitHistory = async ({
   inputs,
@@ -226,25 +225,7 @@ const getChangedFilesFromRESTAPI = async ({
   })
 }
 
-async function validateSubscription(): Promise<void> {
-  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`
-
-  try {
-    await axios.get(API_URL, {timeout: 3000})
-  } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      core.error(
-        'Subscription is not valid. Reach out to support@stepsecurity.io'
-      )
-      process.exit(1)
-    } else {
-      core.info('Timeout or API not reachable. Continuing to next step.')
-    }
-  }
-}
-
 export async function run(): Promise<void> {
-  await validateSubscription()
   core.startGroup('changed-files')
 
   const env = await getEnv()
